edddf68c582b412cccf0b33a9f828df34ec5fe16
D3NCyan <edwarddoltons@gmail.com>
2021-12-27 11:28:42 +0800

    use store with useSelector and add type in DetailNews

--- src/presentation/pages/DetailNews.tsx
+++ src/presentation/pages/DetailNews.tsx
@@ -1,7 +1,31 @@
+import { useSelector } from "react-redux"
+
+interface DetailNewsState {
+    news: {
+        title: string,
+        description: string,
+        image: string,
+        sourceName: string,
+        url: string
+    }
+}
+
 const DetailNews = () => {
+    const { 
+        title,
+        description,
+        image,
+        sourceName,
+        url
+    } = useSelector((state: DetailNewsState) => state.news)
+
+    if (!title || !description) {
+        window.location.href = '/not-found'
+    }
+
     return (
         <div>
-            Hello
+            <img src={image} alt={title} />
         </div>
     )
 }
