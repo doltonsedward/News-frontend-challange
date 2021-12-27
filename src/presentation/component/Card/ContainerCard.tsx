20e51ccb1b4d73a3303c2cbc048865bb688b4c56
D3NCyan <edwarddoltons@gmail.com>
2021-12-27 21:27:13 +0800

    create ContainerCard

---
+++ src/presentation/component/Card/ContainerCard.tsx
@@ -0,0 +1,47 @@
+import { Link } from 'react-router-dom'
+import { 
+    CardGroup,
+    Card,
+    CardImg,
+    CardBody,
+    CardTitle,
+    CardText,
+} from 'reactstrap'
+
+interface Props {
+    data: [],
+    handleDetailNews: Function
+}
+
+const ContainerCard = (props: Props) => {
+    const { data, handleDetailNews } = props
+    return (
+        <CardGroup className="responsive-grid">
+            {data.map((item:any, i:number) => (
+                <Link to="/detail-news" key={i} className="wrapper__card" onClick={() => handleDetailNews(item)}>
+                    <Card>
+                        <div className="wrapper-img-body">
+                            <CardImg
+                                alt={item.title}
+                                src={item.urlToImage}
+                                top
+                                width="100%"
+                            /> 
+                            <span className="source">tes</span>
+                        </div> 
+                        <CardBody>
+                            <CardTitle tag="h3">
+                                {item.title}
+                            </CardTitle>
+                            <CardText>
+                                {item.description}
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                </Link>
+            ))}
+        </CardGroup>
+    )
+}
+
+export default ContainerCard
