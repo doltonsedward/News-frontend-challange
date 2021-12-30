f5cc298bea1cae82bf37aa8f267f6d43e8ad2e6c
D3NCyan <edwarddoltons@gmail.com>
2021-12-30 08:47:12 +0800

    create DetailNewsInterface

---
+++ src/application/common/interface.ts
@@ -0,0 +1,9 @@
+export interface DetailNewsInterface {
+    news: {
+        title: string,
+        description: string,
+        image: string,
+        sourceName: string,
+        url: string
+    }
+}
